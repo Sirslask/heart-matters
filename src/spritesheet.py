927a08ba60d9a6ad1384c31c269d491c8639129c
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2013-01-27 12:33:22 +0100

    Changes to char-anim. does not draw

--- src/spritesheet.py
+++ src/spritesheet.py
@@ -2,6 +2,7 @@
 
 ###
 ### This code shamelessly copied from http://www.pygame.org/wiki/Spritesheet
+### Claiming fair use.
 ###
 
 # This class handles sprite sheets
@@ -31,6 +32,8 @@ class spritesheet(object):
 		if colorkey is not None:
 			if colorkey is -1:
 				colorkey = image.get_at((0,0))
+			elif type(colorkey) not in (pygame.Color,tuple,list):
+				colorkey = image.get_at((colorkey,colorkey))
 			image.set_colorkey(colorkey, pygame.RLEACCEL)
 		return image
 
@@ -47,50 +50,50 @@ class spritesheet(object):
 		return self.images_at(tups, colorkey)
 
 class SpriteStripAnim(object):
-    """sprite strip animator
+	"""sprite strip animator
 
-    This class provides an iterator (iter() and next() methods), and a
-    __add__() method for joining strips which comes in handy when a
-    strip wraps to the next row.
-    """
-    def __init__(self, filename, rect, count, colorkey=None, loop=False, frames=1):
-        """construct a SpriteStripAnim
+	This class provides an iterator (iter() and next() methods), and a
+	__add__() method for joining strips which comes in handy when a
+	strip wraps to the next row.
+	"""
+	def __init__(self, filename, rect, count, colorkey=None, loop=False, frames=1):
+		"""construct a SpriteStripAnim
 
-        filename, rect, count, and colorkey are the same arguments used
-        by spritesheet.load_strip.
+		filename, rect, count, and colorkey are the same arguments used
+		by spritesheet.load_strip.
 
-        loop is a boolean that, when True, causes the next() method to
-        loop. If False, the terminal case raises StopIteration.
+		loop is a boolean that, when True, causes the next() method to
+		loop. If False, the terminal case raises StopIteration.
 
-        frames is the number of ticks to return the same image before
-        the iterator advances to the next image.
-        """
-        self.filename = filename
-        ss = spritesheet.spritesheet(filename)
-        self.images = ss.load_strip(rect, count, colorkey)
-        self.i = 0
-        self.loop = loop
-        self.frames = frames
-        self.f = frames
+		frames is the number of ticks to return the same image before
+		the iterator advances to the next image.
+		"""
+		self.filename = "res/image/"+filename
+		ss = spritesheet(filename)
+		self.images = ss.load_strip(rect, count, colorkey)
+		self.i = 0
+		self.loop = loop
+		self.frames = frames
+		self.f = frames
 
-    def iter(self):
-        self.i = 0
-        self.f = self.frames
-        return self
+	def iter(self):
+		self.i = 0
+		self.f = self.frames
+		return self
 
-    def next(self):
-        if self.i >= len(self.images):
-            if not self.loop:
-                raise StopIteration
-            else:
-                self.i = 0
-        image = self.images[self.i]
-        self.f -= 1
-        if self.f == 0:
-            self.i += 1
-            self.f = self.frames
-        return image
+	def next(self):
+		if self.i >= len(self.images):
+			if not self.loop:
+				raise StopIteration
+			else:
+				self.i = 0
+		image = self.images[self.i]
+		self.f -= 1
+		if self.f == 0:
+			self.i += 1
+			self.f = self.frames
+		return image
 
-    def __add__(self, ss):
-        self.images.extend(ss.images)
-        return self
+	def __add__(self, ss):
+		self.images.extend(ss.images)
+		return self
